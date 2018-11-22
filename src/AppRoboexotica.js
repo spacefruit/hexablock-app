e74e31b2f031799e8aef4e9e59e3000c01a92ec7
Moritz Riede <mriede@tuta.io>
2018-11-22 16:34:00 +0100

    [big] Implement generator functionality; add form data persistency via LocalStorage

--- src/AppRoboexotica.js
+++ src/AppRoboexotica.js
@@ -1,79 +1,52 @@
+/*
+
+  Roboexotica Generator
+  (c) 2018 Wizards at Work GmbH
+  Author: Moritz Riede <mriede@tuta.io>
+
+  History:
+
+  11 Nov 2018:  Create app
+  14 Nov 2018:  Set up controls for the generator
+  22 Nov 2018:  CSS refinements, generator functionality
+
+*/
+
 import React, { Component } from 'react';
+import moment from 'moment'
+
 import logo from './logo.svg';
 import './AppRoboexotica.css';
 
-import SimpleHexGrid from './SimpleHexGrid'
-import RecursiveHexGrid from './RecursiveHexGrid'
-import TwindragonGrid from './TwindragonGrid'
-import TriangleHexGrid from './TriangleHexGrid'
 import RoboexoticaGenerator from './RoboexoticaGenerator'
-
 import story from './story'
-import meSpeak from './mespeak/mespeak.js'
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    let minutesItem = localStorage.getItem('minutes')
+    let promilleItem = localStorage.getItem('promille')
+    let datetimeItem = localStorage.getItem('datetime')
+    let namesItem = localStorage.getItem('names')
+    let notesItem = localStorage.getItem('notes')
+    let seedItem = localStorage.getItem('seed')
+    let typeItem = localStorage.getItem('type')
+
+    // Check if the value has been stored in localStorage before. If yes, use it, if not, fall back to default values.
     this.state = {
       /*depth: 30,
       scale: 1,
       scale2: 0.3,*/
-      minutes: 10,
-      promille: 15,
-      datetime: null,
-      names:    "",
-      notes:    "",
-      seed:     1337,
-      type:     'futuristic',
+      minutes:  minutesItem ? parseFloat(minutesItem) : 10, // 10
+      promille: promilleItem ? parseFloat(promilleItem) : 15, // 15
+      datetime: datetimeItem ? datetimeItem : {},
+      names:    namesItem ? namesItem : "",
+      notes:    notesItem ? notesItem : "",
+      seed:     seedItem  ? seedItem : 1337,
+      type:     typeItem ? typeItem : 'futuristic',
     }
-
-    console.log(story)
   }
-
-  onMinutesChange = (evt) => {
-    this.setState({ minutes: parseFloat(evt.target.value) })
-  }
-
-  onPromilleChange = (evt) => {
-    this.setState({ promille: parseFloat(evt.target.value) })
-  }
-
-  onNamesChange = (evt) => {
-    this.setState({ names: evt.target.value })
-  }
-
-  onNotesChange = (evt) => {
-    this.setState({ notes: evt.target.value })
-  }
-
-  onDateTimeChange = (evt) => {
-    this.setState({ datetime: evt.target.value })
-  }
-
-  onSeedChange = (evt) => {
-    this.setState({ seed: evt.target.value })
-  }
-
-
-
-  onClickExportSVG = (evt) => {
-    let document = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-    document += this.svgRef.innerHTML
-    window.location.href = "data:application/svg+xml;base64," + window.btoa(document)
-  }
-
-  setSvgRef = (elem) => {
-    this.svgRef = elem
-  }
-
-  getSpeechURL = (text, language) => {
-    if (!language) language = "en";
-    var x = "https://translate.google.com/translate_tts?ie=UTF-8&q=" + text +"&tl=" + language;
-    //x = "http://translate.google.com/translate_tts?ie=UTF-8&q=" + text +"&tl=en&total=1&idx=0&textlen=23&prev=input";
-    return x.replace(/ /g, "%20");
-  }
-
-
 
   render() {
     const { minutes, promille, names, notes, datetime, seed, type } = this.state
@@ -88,9 +61,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <img src="waw-logo.svg" className="waw-logo" alt="logo" />
           <h2>Welcome to Roboexotica 2018</h2>
         </div>
-        <div class="controls">
+        <div className="controls">
         minutes {minutes} <input type="range" id="myMinutes" min={1} max={100}
           value={minutes} onChange={this.onMinutesChange}
         />
@@ -109,31 +83,54 @@ class App extends Component {
           <option value="jugendstil">Jugendstil</option>
         </select>
 
-        <button onClick={this.onClickExportSVG}>Export SVG</button>
-
-          {/*<TwindragonGrid depth={depth} scale={scale}/>
+        <button onClick={this.onClickExportSVG}>Print</button>
 
 
-          transform={"translate(" + (x + xOffset) * scaleX) + ", " + (y * scaleY) + ")" }
-
-          */}
-          <br />
-          <div ref={this.setSvgRef}>
-            <svg>
-            
-        {/*
-            <RecursiveHexGrid rows={rows*2} cols={cols*2} lineWidth={scale} hexDiameter={depth/2} transform={t2} deltaX={1} deltaY={-0.5}/>
-
-            <RecursiveHexGrid rows={rows*4} cols={cols*4} lineWidth={scale} hexDiameter={depth/4} transform={t2} deltaX={1} deltaY={-1.5}/>
-
-            <RecursiveHexGrid rows={rows*8} cols={cols*8} lineWidth={scale} hexDiameter={depth/8} transform={t2} deltaX={1} deltaY={-3.5}/>
-            <RecursiveHexGrid rows={rows*16} cols={cols*16} lineWidth={scale} hexDiameter={depth/16} transform={t2} deltaX={1} deltaY={-7.5}/>
-          */}
-            </svg>
-          </div>
+          <RoboexoticaGenerator 
+            minutes={minutes} 
+            promille={promille}
+            names={names}
+            notes={notes}
+            seed={seed}
+            type={type}
+          />
         </div>
       </div>
     );
+  }
+
+  onMinutesChange = (evt) => {
+    this.setState({ minutes: parseFloat(evt.target.value) })
+    localStorage.setItem('minutes', parseFloat(evt.target.value))
+  }
+
+  onPromilleChange = (evt) => {
+    this.setState({ promille: parseFloat(evt.target.value) })
+    localStorage.setItem('promille', parseFloat(evt.target.value))
+  }
+
+  onNamesChange = (evt) => {
+    this.setState({ names: evt.target.value })
+    localStorage.setItem('names', evt.target.value)
+  }
+
+  onNotesChange = (evt) => {
+    this.setState({ notes: evt.target.value })
+    localStorage.setItem('notes', evt.target.value)
+
+  }
+
+  onDateTimeChange = (evt) => {
+    let timestamp = moment(evt.target.value)
+
+    this.setState({ datetime: timestamp })
+    localStorage.setItem('datetime', timestamp)
+
+  }
+
+  onSeedChange = (evt) => {
+    this.setState({ seed: evt.target.value })
+    localStorage.setItem('seed', evt.target.value)
   }
 }
 
--- src/RoboexoticaGenerator.js
+++ src/RoboexoticaGenerator.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import './SimpleHexGrid.css';
+import moment from 'moment'
+
+import './RoboexoticaGenerator.css';
 
 class RoboexoticaGenerator extends Component {
   static propTypes = {
@@ -13,44 +15,31 @@ class RoboexoticaGenerator extends Component {
     type:     PropTypes.string
   }
 
+  formatDate = function (datum) {
+    var date = new Date(datum);
+    return date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
+  }
+
   render() {
-    
-    var d = 20
-    var points = ""
-    var n = 6
-    var cx = 100
-    var cy = 100
-    for (var i = 0; i < n; i++) {
-      let a = (i+0.5) * 2 * Math.PI / n
-
-      let x = cx + d * Math.cos(a)
-      let y = cy + d * Math.sin(a)
-
-      if (i != 0) {
-        points += " "
-      }
-
-      let point = x + "," + y;
-      points += point
-    }
-
     let { minutes, promille, names, notes, datetime, seed, type} = this.props
 
-
-
-
     return (
-      <div className="SimpleHexGrid-">
-        
+      <div className="cert">
+        <h1>Zertifikat</h1>
+        <div className="item">
+          Hiermit wird amtlich bestätigt, dass <b className="names">{names}</b> am
+        </div>
+        <div className="item date">{moment(datetime).format("DD.MM.YYYY")}</div>
+        <div className="item">um</div>
+        <div className="item time">{moment(datetime).format("HH:MM")}</div>
+        <p className="item">
+          mit <b>0.0‰</b> das Experiment begonnen und nach {minutes} Minuten <b>{promille}‰</b> Alkohol in der Atemluft hatte.  
+        </p>
+        <p className="item signature">
+          Wien, am {moment().format("DD.MM.YYYY")}
+        </p>
       </div>
     );
-
-      /* <polyline fill="none" stroke="#5555FF" 
-            strokeWidth={lineWidth}
-            points={points} />
-            */
-    // <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
-
   }
 }
 
